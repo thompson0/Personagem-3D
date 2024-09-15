6c8089523360d51c15722c5e11196504391054a6
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-15 19:41:37 -0300

    Movimento alterado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,39 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Animator animator;
-    private Rigidbody rb;
+    private CharacterController controller; // Componente para controlar o movimento do personagem
+    private Transform myCamera; // Referência à transformação da câmera principal
+    private Animator animator; // Componente para controlar as animações do personagem
 
-    [SerializeField] private float MoveSpeed = 5f;
-    // Start is called before the first frame update
-    void Start()
+    // Inicialização de componentes
+    private void Start()
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>(); // Obtém o componente CharacterController do GameObject
+        myCamera = Camera.main.transform; // Atribui a transformação da câmera principal
+        animator = GetComponent<Animator>(); // Obtém o componente Animator do GameObject
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-       
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
-        float horizontalSpeed = horizontal * MoveSpeed;
-        float verticalSpeed = vertical * MoveSpeed;
-        
-        rb.velocity  = new Vector3(horizontalSpeed,rb.velocity.y,verticalSpeed);
-        transform.forward = new Vector3(rb.velocity.x,0,rb.velocity.z);
-        if (rb.velocity.x != 0 || rb.velocity.z != 0)
+        float horizontal = Input.GetAxis("Horizontal"); // Obtém o input horizontal (A, D, esquerda, direita)
+        float vertical = Input.GetAxis("Vertical"); // Obtém o input vertical (W, S, cima, baixo)
+        Vector3 movimento = new Vector3(horizontal, 0, vertical); // Cria um vetor de movimento baseado nos inputs
+
+        movimento = myCamera.TransformDirection(movimento); // Ajusta o vetor de movimento para corresponder à orientação da câmera
+        movimento.y = 0; // Garante que não há movimento no eixo Y (vertical)
+
+        controller.Move(movimento * Time.deltaTime * 5f); // Aplica o movimento ao personagem
+        controller.Move(new Vector3(0, -9.81f, 0) * Time.deltaTime); // Aplica gravidade ao personagem
+
+        // Rotação do personagem para enfrentar a direção do movimento
+        if (movimento != Vector3.zero)
         {
-            animator.SetBool("isRunning", true);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movimento), Time.deltaTime * 10);
         }
-        else
-        {
-            animator.SetBool("isRunning", false);
-        }
+
+        animator.SetBool("isRunning", movimento != Vector3.zero); // Ativa a animação de movimento se houver deslocamento
     }
 }
